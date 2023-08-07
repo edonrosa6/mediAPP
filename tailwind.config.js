443474960f1e572599c207025a06091d45cff31d
edsonrosales6 <edsonrosales123@gmail.com>
2023-08-07 14:08:18 -0600

    se agrega buscador y se cambian colores grises

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          900: "#1a1a1a",
+          800: "#1f1f1f",
+          700: "#2a2a2a",
+          600: "#3a3a3a",
+          500: "#4d4d4d",
+          200: "#f3f3f3",
+          100: "#ffffff",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -21,5 +32,6 @@ module.exports = {
       },
     },
   },
+
   plugins: [],
 };
